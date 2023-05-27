82ea53f1aa0bf92a0a1f21e4c34fe7cd87ab8dcf
David Avagyan <david_avagyan@list.ru>
2023-05-27 13:55:35 +0300

    Unmute NN & change hash to sha256

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
+import hashlib
+
 import streamlit as st
 
-# from model import DEFAULT_MAX_LEN, MAX_LEN, MIN_LEN, MODEL
+from model import DEFAULT_MAX_LEN, MAX_LEN, MIN_LEN, MODEL
 from topics import QUESTION_TO_OPTIONS
+
+
+def get_hash(text: str) -> int:
+    hash_obj = hashlib.sha256(text.encode())
+    return int(hash_obj.hexdigest(), base=16) & 0xFFFFFFFFFFFFFFFF
 
 
 st.set_page_config(
     page_title="Perfect project topic!",
     page_icon="https://raw.githubusercontent.com/VonNeumann47/perfect-project-topic/main/icon.png",
-    # page_icon="https://github.com/VonNeumann47/perfect-project-topic/blob/main/icon.png?raw=true",
 )
 
 st.title("Узнай идеальную тему своего проекта!")
@@ -24,25 +30,25 @@ for question, options in QUESTION_TO_OPTIONS.items():
     answer = st.text_input(
         label=question,
     ).strip()
-    parts.append(options[hash(answer) % len(options)])
+    parts.append(options[get_hash(answer) % len(options)])
 
-# max_length = st.slider(
-#     "Максимальная длина аннотации (в словах)",
-#     MIN_LEN,
-#     MAX_LEN,
-#     DEFAULT_MAX_LEN,
-#     help="Обычный слайдер длины текста, что такого?",
-# )
+max_length = st.slider(
+    "Максимальная длина аннотации (в словах)",
+    MIN_LEN,
+    MAX_LEN,
+    DEFAULT_MAX_LEN,
+    help="Обычный слайдер длины текста, что такого?",
+)
 
 button = st.button("Хочу ИДЕАЛЬНУЮ тему!")
 
-# MODEL.clear_output()
+MODEL.clear_output()
 if button:
     topic = " ".join(parts).replace(" :", ":")
-    # MODEL.run_model(topic, max_length=max_length)
-    # description = MODEL.get_last_output()
+    MODEL.run_model(topic, max_length=max_length)
+    description = MODEL.get_last_output()
 
     st.markdown("_" * 10)
     st.markdown(f"**Тема проекта**: {topic}")
-    # st.markdown("**Аннотация**")
-    # st.markdown(description)
+    st.markdown("**Аннотация**")
+    st.markdown(description)
